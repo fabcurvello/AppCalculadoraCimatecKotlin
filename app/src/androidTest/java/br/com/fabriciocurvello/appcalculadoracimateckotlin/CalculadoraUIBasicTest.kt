0f16abdca140e8ab4e07c2029f4916ca0ebd5c45
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-09-11 23:13:00 -0300

    Classe Teste Ui CalculadoraUIBasicTest.kt

--- app/src/androidTest/java/br/com/fabriciocurvello/appcalculadoracimateckotlin/CalculadoraUIBasicTest.kt
+++ app/src/androidTest/java/br/com/fabriciocurvello/appcalculadoracimateckotlin/CalculadoraUIBasicTest.kt
@@ -5,14 +5,16 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class CalculadoraUIBasicTest {
 
     @get:Rule
     var activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
-
 
     @Test
     fun testTextFieldInput() {
